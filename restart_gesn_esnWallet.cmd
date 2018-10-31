ef682cd778da29274b3889f55d20a551b8e7073e
Gons <turbobit@closetoya.com>
2018-10-31 14:44:41 +0900

    don not autorun  wallet

--- restart_gesn_esnWallet.cmd
+++ restart_gesn_esnWallet.cmd
@@ -3,4 +3,4 @@ taskkill /F /IM "gesn.exe"
 start /D "%appdata%\ESNwallet\binaries\Gesn\unpacked\" gesn.exe console
 
 
-start /D "D:\wallet_program\ESNwallet0.3.4-win64\ESNwallet0.3.4-win64\" ESNwallet.exe
+::start /D "D:\wallet_program\ESNwallet0.3.4-win64\ESNwallet0.3.4-win64\" ESNwallet.exe
